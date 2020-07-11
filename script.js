d6574ad3239126925674b47563bb19bb83115d85
DevShrimali <devloper.ds@gmail.com>
2020-07-12 02:23:05 +0530

    Update website

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const imageConatainer = document.getElementById('image-container');
-const loader = document.getElementById('loader')
+const loader = document.getElementById('loader');
 
 let photosArray = [];
 
 // Unsplash API
-const count =10;
-const apiKey ='1B7F84af7Hnsx7oFj5qynaFKZySqAw_odxUP0V5ZDsw';
-const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+const count = 10;
+const apiKey = '1B7F84af7Hnsx7oFj5qynaFKZySqAw_odxUP0V5ZDsw';
+const apiUrl = `https://api.unsplash.com/photos/?client_id=${apiKey}&count=${count}`;
 
 // Helper Function to Set Attributes on DOM Elements 
 function setAttributes (element, attributes) {
@@ -21,33 +21,33 @@ function displayPhotos() {
     photosArray.forEach((photo) => {
         // Create <a> to link to unsplash
         const item = document.createElement('a');
-        setAttributes(item, {
-            href: photo.link.html,
-            target: '_blank',
-        });
-        // Create <img> for photo
+        item.setAttribute('href', photo.links.html);
+        item.setAttribute('target', '_blank');
+        // Create <img>
         const img = document.createElement('img');
-        
-        setAttributes(img, {
-            scr: photo.urls.regular,
-            alt: photo.alt_description,
-            title: photo.alt_description,
-        });
-        // Put <img> inside <a>, then put both inside imageContainer Element
+        img.setAttribute('src', photo.urls.regular);
+        img.setAttribute('alt', photo.alt_description);
+        img.setAttribute('title', photo.alt_description);
+        // Put <img> inside <a>
         item.appendChild(img);
         imageConatainer.appendChild(item);
-    }); 
-} 
+
+    });
+}
 // Get Photos from Unsplash API
 async function getPhotos() {
     try {
         const response = await fetch(apiUrl);
         photosArray = await response.json();
+        //console.log(photosArray);
         displayPhotos();
+        
     } catch (error) {
         // Catch Error Here
+        console.log(error);
     }
 }
 
 // On Load 
+//displayPhotos();
 getPhotos();
